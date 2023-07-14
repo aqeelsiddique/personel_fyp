c5d52e38ae23240ed157a57271ee54b81c5c30f0
Aqeel <aqeelsaddique83@gmail.com>
2023-07-14 23:09:08 +0500

    testing

--- route/route.js
+++ route/route.js
@@ -626,4 +626,24 @@ app.get("/subjects", async (req, res) => {
   });
 
   //////////////////////////////////End OF Result Code////////////////////
+
+
+
+
+
+
+
+app.get('/weather', (req, res) => {
+	let city = req.query.city;
+	var request = require('request');
+	request(
+		`https://samples.openweathermap.org/data/2.5/forecast?q=${city}&appid=replace-this-with-your-api-key`,
+		function(error, response, body) {
+			let data = JSON.parse(body);
+			if (response.statusCode === 200) {
+				res.send(`The weather in your city "${city}" is ${data.list[0].weather[0].description}`);
+			}
+		}
+	);
+});
 };
